8029232a6768aea5b504dea91480473cb6a919d4
Kwikl-Client <159225394+Kwikl-Client@users.noreply.github.com>
2024-04-02 09:26:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ app.use("/customer", CustomerRoutes);
 app.use("/payment", PaymentRoutes);
 app.use("/cms", ContentRoutes);
 
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`.bold.brightGreen);
