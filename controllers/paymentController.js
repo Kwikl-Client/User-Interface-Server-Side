a2f00b412125e6d44d727a629cc403a1f9a24215
Kwikl-Client <159225394+Kwikl-Client@users.noreply.github.com>
2024-05-07 16:14:23 +0530

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -37,7 +37,6 @@ export const createPaymentIntent = async (req, res) => {
             currency: 'USD',
             product_data: {
               name: "Salssky - The Chosen One",
-              description: "Note: Your card statement will reflect a charge from Kwik L Inc.",
             },
             unit_amount: unitAmount
           },
