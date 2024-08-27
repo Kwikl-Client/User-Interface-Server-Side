85403a58487b26b814a4a7b5d4fce04542998cca
Kwikl-Client <159225394+Kwikl-Client@users.noreply.github.com>
2024-08-27 10:09:16 +0530

    Update allUsersModel.js

--- models/allUsersModel.js
+++ models/allUsersModel.js
@@ -1,1 +1,14 @@
+import mongoose from 'mongoose';
 
+const UsersSchema = mongoose.Schema(
+    { 
+      email: {
+        type: String,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+const usersModel = mongoose.model('users', UsersSchema, 'users');
+export default usersModel;
