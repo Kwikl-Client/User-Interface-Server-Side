8a681c21c254abb466a83d71b1d12b4677d52274
Kwikl-Client <159225394+Kwikl-Client@users.noreply.github.com>
2024-09-26 12:52:09 +0530

    Update customerRoutes.js clean code

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { allUsersData,registerCustomer, loginCustomer,acceptCookiePolicy, getAllCustomers,editCustomerDetails, raiseCommunityRequest, verifyTkn, checkEmail, forgotPassword} from '../controllers/customerController.js';
+import { allUsersData,registerCustomer, loginCustomer,acceptCookiePolicy,editCustomerDetails, raiseCommunityRequest, verifyTkn, checkEmail, forgotPassword} from '../controllers/customerController.js';
 import { protect } from '../middlewares/authmiddlewares.js';
 import uploadImg from '../middlewares/uploadFilemiddlewares.js';
 import upload from '../middlewares/multermiddlewares.js';
@@ -9,7 +9,6 @@ const CustomerRoutes = express.Router();
 CustomerRoutes.post('/allUsers', allUsersData);
 CustomerRoutes.post('/registerCustomer', registerCustomer);
 CustomerRoutes.post('/loginCustomer', loginCustomer);
-CustomerRoutes.post('/getAllCustomer', getAllCustomers);
 CustomerRoutes.get('/verifyTkn/:token', verifyTkn);
 CustomerRoutes.post('/checkUser', checkEmail);
 CustomerRoutes.patch('/editCustomerDetails', 
