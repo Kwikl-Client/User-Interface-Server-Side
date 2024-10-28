883fedcf0c03e5f0fe97070cb700754a3d6bb8e8
Kwikl-Client <159225394+Kwikl-Client@users.noreply.github.com>
2024-10-28 10:13:39 +0530

    Update vistorsModel.js

--- models/vistorsModel.js
+++ models/vistorsModel.js
@@ -4,6 +4,7 @@ const visitorSchema = new mongoose.Schema({
   date: { type: String, unique: true },  // Store date as string in "YYYY-MM-DD" format
   count: { type: Number, default: 0 },   // Visitor count
   likes: { type: Number, default: 0 },   // Likes count
+  visitors: { type: [String], default: [] } // Array to store unique visitor IPs
 });
 
 const Visitor = mongoose.model('Visitor', visitorSchema);
