87ab24efd3bcaf28d6b523667023c3706f371117
Kwikl-Client <159225394+Kwikl-Client@users.noreply.github.com>
2025-07-30 11:06:34 +0530

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createPaymentIntentForBook, retrievePaymentDetails, createSubscription,
+import { createPaymentIntentForBook, retrievePaymentDetails,
     updateSubscription,retrieveSubscriptionDetails,cancelSubscription, PaymentIntentToAuthor } from '../controllers/paymentController.js';
     // updatePaymentStatus, totalRevenue, revenueInDateRange, refund, 
 
@@ -7,7 +7,7 @@ const PaymentRoutes = express.Router();
 
 PaymentRoutes.get('/retrievePaymentDetails/:sessionId', retrievePaymentDetails);
 PaymentRoutes.get('/retrieveSubscriptionDetails/:subscriptionId', retrieveSubscriptionDetails);
-PaymentRoutes.post('/create-subscription', createSubscription);
+// PaymentRoutes.post('/create-subscription', createSubscription);
 PaymentRoutes.get('/createPaymentIntent', createPaymentIntentForBook);
 PaymentRoutes.post('/payment-talkToAuthor', PaymentIntentToAuthor);
 PaymentRoutes.delete('/cancelPaymentIntent/:paymentIntentId', cancelSubscription);
