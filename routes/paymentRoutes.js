8f02042d95321e1768864967251f99c0956407b2
Kwikl-Client <159225394+Kwikl-Client@users.noreply.github.com>
2025-11-19 18:23:35 +0530

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,19 +1,19 @@
-
 import express from 'express';
-import { createPaymentIntentForBook, retrievePaymentDetails,
-    updateSubscription,retrieveSubscriptionDetails,cancelSubscription, PaymentIntentToAuthor } from '../controllers/paymentController.js';
+import { createPaymentIntentForBook, retrievePaymentDetails, createSubscription,
+    updateSubscription,retrieveSubscriptionDetails,cancelSubscription, PaymentIntentToAuthor, 
+    renewalCheckoutSession,renewSamePlan} from '../controllers/paymentController.js';
     // updatePaymentStatus, totalRevenue, revenueInDateRange, refund, 
 
 const PaymentRoutes = express.Router();
 
 PaymentRoutes.get('/retrievePaymentDetails/:sessionId', retrievePaymentDetails);
 PaymentRoutes.get('/retrieveSubscriptionDetails/:subscriptionId', retrieveSubscriptionDetails);
-// PaymentRoutes.post('/create-subscription', createSubscription);
+PaymentRoutes.post('/create-subscription', createSubscription);
+PaymentRoutes.get('/renewal-subscription', renewalCheckoutSession);
 PaymentRoutes.get('/createPaymentIntent', createPaymentIntentForBook);
 PaymentRoutes.post('/payment-talkToAuthor', PaymentIntentToAuthor);
 PaymentRoutes.delete('/cancelPaymentIntent', cancelSubscription);
 PaymentRoutes.patch('/update-subscription', updateSubscription);
-
+PaymentRoutes.get("/renew-same", renewSamePlan);
 
 export default PaymentRoutes;
-
